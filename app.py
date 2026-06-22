d5df625885982e4fe9bd82f843629974881c3223
Harsh Singla <harshsingla74@gmail.com>
2026-06-22 18:32:31 +0530

    updated

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template, redirect, url_for
-
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    request
+)
 from database.db import db
 from database import models
 
 from database.models import Interview
 
+from utils.resume_parser import extract_text_from_pdf
 
+import os
+
+
+
+UPLOAD_FOLDER = "uploads"
+
+ALLOWED_EXTENSIONS = {"pdf"}
 
 app = Flask(__name__)
 
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///interview.db"
 
@@ -18,6 +32,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
+def allowed_file(filename):
+
+    return (
+        "." in filename
+        and
+        filename.rsplit(".", 1)[1].lower()
+        in ALLOWED_EXTENSIONS
+    )
 
 @app.route("/")
 def home():
@@ -31,9 +53,66 @@ def home():
 @app.route("/dashboard")
 def dashboard():
 
+    interviews = Interview.query.all()
+
     return render_template(
-        "dashboard.html"
+        "dashboard.html",
+        interviews=interviews
     )
+
+@app.route(
+    "/upload-resume/<int:interview_id>",
+    methods=["POST"]
+)
+def upload_resume(interview_id):
+
+
+    if "resume" not in request.files:
+
+        return "No File Selected"
+
+
+    file = request.files["resume"]
+
+
+    if file.filename == "":
+
+        return "No File Selected"
+
+
+    if file and allowed_file(file.filename):
+
+
+        filename = file.filename
+
+
+        save_path = os.path.join(
+            app.config["UPLOAD_FOLDER"],
+            filename
+        )
+
+
+        file.save(save_path)
+
+
+        interview = Interview.query.get(
+            interview_id
+        )
+
+
+        if interview:
+
+            interview.resume = save_path
+
+            db.session.commit()
+
+
+        return redirect(
+            url_for("dashboard")
+        )
+
+
+    return "Only PDF files allowed"
 
 
 
@@ -91,6 +170,33 @@ def result():
 
     return render_template(
         "result.html"
+    )
+
+
+
+@app.route("/resume-analysis/<int:interview_id>")
+def resume_analysis(interview_id):
+
+
+    interview = Interview.query.get_or_404(
+        interview_id
+    )
+
+
+    if not interview.resume:
+
+        return "Resume not uploaded"
+
+
+    extracted_text = extract_text_from_pdf(
+        interview.resume
+    )
+
+
+    return render_template(
+        "resume_analysis.html",
+        extracted_text=extracted_text,
+        interview=interview
     )
 
 
---
+++ utils/resume_parser.py
@@ -0,0 +1,14 @@
+import fitz
+
+def extract_text_from_pdf(pdf_path):
+
+    text = ""
+
+    pdf = fitz.open(pdf_path)
+
+    for page in pdf:
+        text += page.get_text("text")
+
+    pdf.close()
+
+    return text
