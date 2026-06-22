d5333f8090810c53f47350a597b4e5aeec608ee9
Harsh Singla <harshsingla74@gmail.com>
2026-06-22 17:15:04 +0530

    Created Flask routes and application pages

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
+# Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+# Login Page
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+# Dashboard Page
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+# Interview Page
+@app.route("/interview")
+def interview():
+    return render_template("interview.html")
+
+
+# Result Page
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
 
 
 if __name__ == "__main__":
